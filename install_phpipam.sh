a91e79214f9ca48ba43ce62a767604aa01fe08dd
SynMan <RubenFilipe@users.noreply.github.com>
2015-03-07 18:36:45 +0000

    Update install_phpipam.sh

--- install_phpipam.sh
+++ install_phpipam.sh
@@ -1,18 +1,27 @@
 
+##Update & Upgrade
 
-wget http://downloads.sourceforge.net/project/phpipam/phpipam-1.1.010.tar
-tar -xvf phpipam-1.1.010.tar /var/www
+apt-get update
 
-## Install Apache
+apt-get upgrade
 
-apt-get install -y apache2 php5 libapache2-mod-php5 php5-gmp php-pear mysql-server libapache2-mod-auth-mysql php5-mysql libgmp-dev
 
-apt-get install -y php-pear php5-cli php5-common php5-curl php5-dev php5-gd php5-mcrypt php5-mysql php5-pgsql php5-xdebug
+## Install Apache , MySQL
+
+apt-get install -y apache2 php5 php5-mysql libapache2-mod-php5 mysql-server libapache2-mod-auth-mysql libgmp-dev
+
+## Install PHP
+
+apt-get install -y php-pear php5-dev php5-cli php5-common php5-curl php5-gmp php5-dev php5-ldap php5-gd php5-mcrypt php5-pgsql php5-xdebug
 
 sudo service apache2 restart
-
-sudo apt-get install -y php5-dev php5-cli php-pear
 
 aptitude install -y libapache2-mod-php5filter 
 
 sudo service apache2 restart
+
+##Download and move
+
+wget http://downloads.sourceforge.net/project/phpipam/phpipam-1.1.010.tar
+tar -xf phpipam-1.1.010.tar 
+mv phpipam /var/www
